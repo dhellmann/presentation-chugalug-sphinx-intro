33c4c1c3818013919a0ef189157433cab89a7cd9
Doug Hellmann <doug.hellmann@gmail.com>
2009-09-18 07:30:42 -0400

    release 1.0

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,7 @@ copyright = u'2009, Doug Hellmann'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
